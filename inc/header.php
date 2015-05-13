09976ed6f275c0081f447abd838725261208b5c5
iMac Andy Mineral <andy@mineral.co.id>
2015-05-13 16:32:58 +0700

    update

--- inc/header.php
+++ inc/header.php
@@ -7,7 +7,7 @@
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</button>
-			<a href="../" class="navbar-brand">YASIN.CO</a>
+			<a href="../" class="navbar-brand">YASIN</a>
 		</div>
 	<nav class="collapse navbar-collapse bs-navbar-collapse">
 		<ul class="filter nav navbar-nav">
