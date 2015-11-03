94488af243bae4569879f476b8ce81efc6c284d2
iMac Andy Mineral <andy@mineral.co.id>
2015-11-03 16:04:50 +0700

    change to simple html

--- inc/footer.php
+++
@@ -1,6 +0,0 @@
-<div id="footer">
-	<div class="container text-center">
-		<p>Made with love by <a href="http://mineral.id" target="_blank">Mineral.id</a></p>
-		<p>Other Project : <a href="http://shalat.co" target="_blank">Shalat.co</a></p>
-	</div>
-</div><!-- #footer -->
